18f5d8d5705245777d637f6a826de147f38c360e
Gabriel Pozato <nathangabriel200@gmail.com>
2022-05-13 16:55:53 -0300

    atualizando os arquivos de logica

--- script.js
+++ script.js
@@ -31,3 +31,30 @@ for(var i = num1; i <= num2; i++){
     }
 }
 console.log("Par: "+ par+ "\n" + "Impar: " +impar);
+
+// 4) Faça um algoritmo que imprima a soma dos números multiplos de 11 em um intervalo digitado pelo usuário;
+
+var num1 = parseInt(prompt("Digite algum numero: "));
+var num2 = parseInt(prompt("Digite outro numero: "));
+
+var num = 0;
+
+for(var i = num1; i <= num2; i++){
+    if(i % 11 == num){
+        num += i;
+    }
+}
+console.log(num)
+
+// 5) Faça um algoritmo que imprima o somatório de todos os números entre 1 e um valor digitado pelo usuário. 
+// Por exemplo, se o usuário digitar o número 4, o programa deverá calcular o somatório 1 + 2 + 3 + 4 = 10.
+
+var num1 = parseInt(prompt("Digite algum numero: "));
+
+var soma = 0;
+
+for(var i = 1; i <= num1; i++){
+    soma = soma + i
+    
+}
+console.log(soma)
